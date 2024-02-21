5588d0987b6b0c9e13eec6c7f15179b0413f4e6c
Zak Bowyer <zak.bowyer@marks-and-spencer.com>
2024-02-21 13:53:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def index():
 def authenticate():
     return jsonify({"message": "Authenticated successfully."}), 200
 
+@app.route('/api/saml2/authenticate/auth0-saml')
+def malicious():
+    return jsonify({"message": "I am malicious!"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
