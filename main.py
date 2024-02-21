190a063199484c85ea6b917b2dac9ce64040830b
Zak Bowyer <zak.bowyer@marks-and-spencer.com>
2024-02-21 13:43:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/api/authenticated')
+    return jsonify({"message": "Authenticated successfully."}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
