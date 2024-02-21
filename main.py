cef025e060b5b7978bd6abbca3b539169a4bbe6b
Zak Bowyer <zak.bowyer@marks-and-spencer.com>
2024-02-21 13:58:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 import os
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -10,11 +12,17 @@ def index():
 
 @app.route('/api/authenticated')
 def authenticate():
+    app.logger.info("Authenticate function called")
     return jsonify({"message": "Authenticated successfully."}), 200
 
 @app.route('/api/saml2/authenticate/auth0-saml')
 def malicious():
+    app.logger.info("Malicious function called")
     return jsonify({"message": "I am malicious!"}), 200
 
 if __name__ == '__main__':
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+
     app.run(debug=True, port=os.getenv("PORT", default=5000))
